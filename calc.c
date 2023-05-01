ed10927f17af15ea0d62cb5f6b6385144ac6fca8
Tripple7souls <tripple7souls@gmail.com>
2023-05-01 11:32:20 +0100

    Calculator

---
+++ calc.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+    double digit1;
+    double digit2;
+    char symbol;
+    double result;
+
+    printf("Enter value: ");
+    scanf("%lf", &digit1);
+
+    printf("Enter value: ");
+    scanf("%lf", &digit2);
+
+    printf("Enter symbol (+ _ * /): ");
+    scanf("%c", &symbol);
+    
+    switch(symbol)
+    {
+        case '+':
+            result = digit1 + digit2;
+            printf("Equals to : %lf\n", result);
+            break;
+        case '-':
+            result = digit1 - digit2;
+            printf("Equals to : %lf\n", result);
+            break;
+        case '*':
+            result = digit1 * digit2;
+            printf("Equals to : %lf\n", result);
+            break;
+        case '/':
+            result = digit1 / digit2;
+            printf("Equals to : %lf \n", result);
+            break;
+        default:
+            printf("Syntax Error");
+            break;
+    }
+    return 0;
+}
